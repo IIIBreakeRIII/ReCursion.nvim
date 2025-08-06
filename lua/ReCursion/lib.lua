3f569d7723f18b7a1b61dc3ca6508352f592f24f
IIIBreakeRIII <ryu990305@icloud.com>
2025-08-06 20:28:34 +0900

    SYNC

--- lua/ReCursion/lib.lua
+++ lua/ReCursion/lib.lua
@@ -2,38 +2,33 @@
 local M = {}
 local result_bufnr, result_winid
 
--- Run a shell command and capture output (captures stderr as well)
+-- Run a shell command and capture both stdout and stderr
 local function run_cmd(cmd)
-  -- Redirect stderr to stdout for debugging
-  local h = io.popen(cmd .. " 2>&1")
-  local out = h:read("*a")
-  h:close()
-  return out
+  local handle = io.popen(cmd .. " 2>&1")
+  local result = handle:read("*a")
+  handle:close()
+  return result
 end
 
--- Open or reuse result window
+-- Open or reuse a scratch window for results
 local function open_window(ft)
   if result_bufnr and vim.api.nvim_buf_is_valid(result_bufnr)
-     and result_winid and vim.api.nvim_win_is_valid(result_winid)
-  then
+     and result_winid and vim.api.nvim_win_is_valid(result_winid) then
     vim.api.nvim_set_current_win(result_winid)
     vim.api.nvim_buf_set_lines(result_bufnr, 0, -1, false, {})
   else
     vim.cmd("rightbelow vsplit")
     result_winid = vim.api.nvim_get_current_win()
     vim.cmd("enew")
-    result_bufnr  = vim.api.nvim_get_current_buf()
+    result_bufnr = vim.api.nvim_get_current_buf()
   end
   vim.bo[result_bufnr].filetype   = ft
   vim.bo[result_bufnr].buftype    = "nofile"
   vim.bo[result_bufnr].bufhidden  = "hide"
   vim.bo[result_bufnr].swapfile   = false
   vim.bo[result_bufnr].modifiable = true
-
-  -- Close buffer shortcut
-  vim.keymap.set("n", ";bd", function()
-    vim.cmd("bd")
-  end, { buffer = result_bufnr, silent = true, noremap = true })
+  vim.keymap.set("n", ";bd", function() vim.cmd("bd") end,
+                 { buffer = result_bufnr, silent = true, noremap = true })
 end
 
 -- Make buffer read-only
@@ -43,99 +38,100 @@ local function finalize()
   end
 end
 
--- Disassemble object file (.o) with temp symbols and basic assembly
+-- Compile helper: source -> output (.o or exe)
+local function compile(source, output, is_executable)
+  local cmd = {"gcc", "-g", "-O0", "-fno-builtin"}
+  if is_executable then
+    table.insert(cmd, "-o"); table.insert(cmd, output)
+    table.insert(cmd, source)
+  else
+    table.insert(cmd, "-c"); table.insert(cmd, source)
+    table.insert(cmd, "-o"); table.insert(cmd, output)
+  end
+  return run_cmd(table.concat(cmd, " "))
+end
+
+-- Disassemble object file (.o) with temp symbols
 function M.disasmObj()
   local name = vim.fn.expand("%:t:r")
-  local tmp_c = "/tmp/recursion_code.c"
-  local tmp_o = "/tmp/recursion_code.o"
-
+  local tmp_c, tmp_o = "/tmp/recursion_code.c", "/tmp/recursion_code.o"
   vim.cmd("write! " .. tmp_c)
-  run_cmd("gcc -g -O0 -fno-builtin -c " .. tmp_c .. " -o " .. tmp_o)
-
-  -- Determine correct disassembly tool: objdump or otool for Mach-O
-  local asm_cmd = "objdump -d -l -S " .. tmp_o
-  local info = run_cmd("file " .. tmp_o)
-  if info:match("Mach%-O") then
-    asm_cmd = "otool -tvV " .. tmp_o
-  end
+  compile(tmp_c, tmp_o, false)
+  -- Use otool on macOS, objdump otherwise
+  local file_info = run_cmd("file " .. tmp_o)
+  local asm_cmd = file_info:match("Mach%-O") and "otool -tvV " .. tmp_o
+                                           or "objdump -d -l -S " .. tmp_o
   local asm = run_cmd(asm_cmd)
-
   open_window("asm")
   vim.api.nvim_buf_set_lines(result_bufnr, 0, -1, false, vim.split(asm, "\n"))
   vim.api.nvim_buf_set_name(result_bufnr, name .. "-ObjDisasm")
-
   finalize()
 end
 
--- Disassemble full linked executable with PLT/GOT (shows external calls)
+-- Disassemble full linked executable with PLT/GOT
 function M.disasmExe()
-  local name   = vim.fn.expand("%:t:r")
-  local tmp_c   = "/tmp/recursion_code.c"
-  local tmp_exe = "/tmp/recursion_code_exe"
-
+  local name = vim.fn.expand("%:t:r")
+  local tmp_c, tmp_exe = "/tmp/recursion_code.c", "/tmp/recursion_code_exe"
   vim.cmd("write! " .. tmp_c)
-  run_cmd("gcc -g -O0 -fno-builtin -o " .. tmp_exe .. " " .. tmp_c)
-
-  -- Determine correct disassembly tool: objdump or otool for Mach-O
-  local asm_cmd = "objdump -d -l -S " .. tmp_exe
-  local info    = run_cmd("file " .. tmp_exe)
-  if info:match("Mach%-O") then
-    asm_cmd = "otool -tvV " .. tmp_exe
-  end
+  compile(tmp_c, tmp_exe, true)
+  local file_info = run_cmd("file " .. tmp_exe)
+  local asm_cmd = file_info:match("Mach%-O") and "otool -tvV " .. tmp_exe
+                                           or "objdump -d -l -S " .. tmp_exe
   local asm = run_cmd(asm_cmd)
-
   open_window("asm")
   vim.api.nvim_buf_set_lines(result_bufnr, 0, -1, false, vim.split(asm, "\n"))
   vim.api.nvim_buf_set_name(result_bufnr, name .. "-FullDisasm")
-
   finalize()
 end
 
 -- Decompile object file (.o) to C using RetDec
 function M.decompileObj()
-  local name   = vim.fn.expand("%:t:r")
-  local tmp_c  = "/tmp/recursion_code.c"
-  local tmp_o  = "/tmp/recursion_code.o"
+  local name = vim.fn.expand("%:t:r")
+  local tmp_c, tmp_o = "/tmp/recursion_code.c", "/tmp/recursion_code.o"
   local tmp_dc = "/tmp/recursion_code_obj_decompiled.c"
-
   vim.cmd("write! " .. tmp_c)
-  run_cmd("gcc -g -O0 -fno-builtin -c " .. tmp_c .. " -o " .. tmp_o)
-  run_cmd("retdec-decompiler --keep-library-funcs --output " .. tmp_dc .. " " .. tmp_o)
-
+  compile(tmp_c, tmp_o, false)
+  local output = run_cmd("retdec-decompiler --keep-library-funcs --output " .. tmp_dc .. " " .. tmp_o)
+  if not vim.loop.fs_stat(tmp_dc) then
+    open_window("text")
+    vim.api.nvim_buf_set_lines(result_bufnr, 0, -1, false, vim.split(output, "\n"))
+    vim.api.nvim_buf_set_name(result_bufnr, name .. "-ObjDecompileError")
+    return finalize()
+  end
   local dc = run_cmd("cat " .. tmp_dc)
-
   open_window("c")
   vim.api.nvim_buf_set_lines(result_bufnr, 0, -1, false, vim.split(dc, "\n"))
   vim.api.nvim_buf_set_name(result_bufnr, name .. "-ObjDecompiled")
-
   finalize()
 end
 
 -- Decompile full executable to C using RetDec
 function M.decompileExe()
-  local name   = vim.fn.expand("%:t:r")
-  local tmp_c  = "/tmp/recursion_code.c"
-  local tmp_exe= "/tmp/recursion_code_exe"
+  local name = vim.fn.expand("%:t:r")
+  local tmp_c, tmp_exe = "/tmp/recursion_code.c", "/tmp/recursion_code_exe"
   local tmp_dc = "/tmp/recursion_code_exe_decompiled.c"
-
   vim.cmd("write! " .. tmp_c)
-  run_cmd("gcc -g -O0 -fno-builtin -o " .. tmp_exe .. " " .. tmp_c)
-  run_cmd("retdec-decompiler --keep-library-funcs --output " .. tmp_dc .. " " .. tmp_exe)
-
+  compile(tmp_c, tmp_exe, true)
+  local output = run_cmd("retdec-decompiler --keep-library-funcs --output " .. tmp_dc .. " " .. tmp_exe)
+  if not vim.loop.fs_stat(tmp_dc) then
+    open_window("text")
+    vim.api.nvim_buf_set_lines(result_bufnr, 0, -1, false, vim.split(output, "\n"))
+    vim.api.nvim_buf_set_name(result_bufnr, name .. "-FullDecompileError")
+    return finalize()
+  end
   local dc = run_cmd("cat " .. tmp_dc)
-
   open_window("c")
   vim.api.nvim_buf_set_lines(result_bufnr, 0, -1, false, vim.split(dc, "\n"))
   vim.api.nvim_buf_set_name(result_bufnr, name .. "-FullDecompiled")
-
   finalize()
 end
 
+-- Register user commands
 function M.setup()
-  vim.api.nvim_create_user_command("ReCDisasmObj",     M.disasmObj,     {})
-  vim.api.nvim_create_user_command("ReCDisasmExe",     M.disasmExe,     {})
-  vim.api.nvim_create_user_command("ReCDecompileObj",  M.decompileObj,  {})
-  vim.api.nvim_create_user_command("ReCDecompileExe",  M.decompileExe,  {})
+  vim.api.nvim_create_user_command("ReCDisasmObj",    M.disasmObj,    {})
+  vim.api.nvim_create_user_command("ReCDisasmExe",    M.disasmExe,    {})
+  vim.api.nvim_create_user_command("ReCDecompileObj", M.decompileObj, {})
+  vim.api.nvim_create_user_command("ReCDecompileExe", M.decompileExe, {})
 end
 
 return M
